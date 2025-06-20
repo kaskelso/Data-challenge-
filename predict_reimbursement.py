99b66775e3366a4ce25edf0d65ba3eaf25d72c98
Kenny Askelson <askelson@zoology.ubc.ca>
2025-06-20 13:13:32 -0700

    add python scripts used to call models

---
+++ predict_reimbursement.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 19 14:48:29 2025
+
+@author: kennyaskelson
+"""
+
+import sys
+import numpy as np
+from tensorflow.keras.models import load_model
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # Suppress INFO, WARNING, and ERROR logs
+
+# Load the model
+model = load_model("reimbursement_model.keras")
+
+# Read command-line arguments
+trip_duration = float(sys.argv[1])
+miles_traveled = float(sys.argv[2])
+total_receipts = float(sys.argv[3])
+
+# Make prediction
+X = np.array([[trip_duration, miles_traveled, total_receipts]])
+prediction = model.predict(X, verbose=0)
+
+# Print the predicted value
+print(round(float(prediction[0]), 2), file=sys.stdout)
